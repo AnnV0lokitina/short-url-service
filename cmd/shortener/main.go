648e72e1290f4d3ab94bee80adfcbe1e0c12b19d
a.volokitina <a.volokitina@tradesoft.ru>
2022-02-11 21:13:20 +0300

    test(shotener) add tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/AnnV0lokitina/short-url-service.git/internal/app"
 	handlerPkg "github.com/AnnV0lokitina/short-url-service.git/internal/handler"
 	"github.com/AnnV0lokitina/short-url-service.git/internal/repo"
 	"github.com/AnnV0lokitina/short-url-service.git/internal/usecase"
@@ -11,6 +10,6 @@ func main() {
 	repository := repo.NewRepo()
 	services := usecase.NewUsecase(repository)
 	handler := handlerPkg.NewHandler(services)
-	application := app.NewApp(handler)
+	application := NewApp(handler)
 	application.Run()
 }
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -2,14 +2,11 @@ package repo
 
 import (
 	"errors"
-	"sync"
-
 	"github.com/AnnV0lokitina/short-url-service.git/internal/entity"
 )
 
 type Repo struct {
 	list map[string]string
-	mu   sync.Mutex
 }
 
 func NewRepo() *Repo {
@@ -20,15 +17,10 @@ func NewRepo() *Repo {
 }
 
 func (r *Repo) SetURL(url *entity.URL) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
 	r.list[url.GetUUID()] = url.GetFullURL()
 }
 
 func (r *Repo) GetURL(uuid string) (*entity.URL, error) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
 	fullURL, ok := r.list[uuid]
 	if !ok {
 		return nil, errors.New("no url saved")

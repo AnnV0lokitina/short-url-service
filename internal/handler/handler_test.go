0c538a411c3c9e442ccce140a4ac4a7582862a85
a.volokitina <a.volokitina@tradesoft.ru>
2022-02-12 15:06:58 +0300

    test(handler): hix thhp test

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -33,7 +33,7 @@ func (u *MockedUsecase) GetURL(uuid string) (*entity.URL, error) {
 	return nil, errors.New("no url saved")
 }
 
-func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) *http.Response {
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
@@ -45,11 +45,7 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io
 	resp, err := client.Do(req)
 	require.NoError(t, err)
 
-	defer resp.Body.Close()
-	respBody, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-
-	return resp, string(respBody)
+	return resp
 }
 
 func TestHandler_ServeHTTP(t *testing.T) {
@@ -159,16 +155,20 @@ func TestHandler_ServeHTTP(t *testing.T) {
 		},
 	}
 
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ts := httptest.NewServer(handler)
-			defer ts.Close()
-
-			resp, body := testRequest(t, ts, tt.request.method, tt.request.target, tt.request.body)
+			resp := testRequest(t, ts, tt.request.method, tt.request.target, tt.request.body)
 			assert.Equal(t, tt.result.code, resp.StatusCode)
 
+			defer resp.Body.Close()
+			respBody, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+
 			if tt.result.body != "" {
-				assert.Equal(t, tt.result.body, body)
+				assert.Equal(t, tt.result.body, string(respBody))
 			}
 
 			if tt.result.contentType != "" {
@@ -177,111 +177,6 @@ func TestHandler_ServeHTTP(t *testing.T) {
 		})
 	}
 }
-
-//func TestHandler_ServeHTTP(t *testing.T) {
-//	repository := repo.NewRepo()
-//	services := usecase.NewUsecase(repository)
-//	handler := NewHandler(services)
-//
-//	fullURL := "http://xfrpm.ru/ovxnqqxiluncj/lqhza6knc6t2m"
-//
-//	url := entity.NewURL(fullURL, "")
-//	url.CreateUUID()
-//
-//	type result struct {
-//		body           string
-//		headerLocation string
-//		code           int
-//		contentType    string
-//	}
-//	tests := []struct {
-//		name    string
-//		request *http.Request
-//		result  result
-//	}{
-//		{
-//			name:    "test create incorrect method",
-//			request: httptest.NewRequest(http.MethodPut, "/", nil),
-//			result: result{
-//				body:           "",
-//				headerLocation: "",
-//				code:           http.StatusBadRequest,
-//				contentType:    "",
-//			},
-//		},
-//		{
-//			name:    "test create url no body",
-//			request: httptest.NewRequest(http.MethodPost, "/", nil),
-//			result: result{
-//				body:           "",
-//				headerLocation: "",
-//				code:           http.StatusBadRequest,
-//				contentType:    "",
-//			},
-//		},
-//		{
-//			name:    "test create url incorrect url",
-//			request: httptest.NewRequest(http.MethodPost, "/", strings.NewReader("////%%%%%%")),
-//			result: result{
-//				body:           "",
-//				headerLocation: "",
-//				code:           http.StatusBadRequest,
-//				contentType:    "",
-//			},
-//		},
-//		{
-//			name:    "test create url positive",
-//			request: httptest.NewRequest(http.MethodPost, "/", strings.NewReader(fullURL)),
-//			result: result{
-//				body:           url.GetShortURL(),
-//				headerLocation: "",
-//				code:           http.StatusCreated,
-//				contentType:    "text/plain; charset=UTF-8",
-//			},
-//		},
-//		{
-//			name:    "test read url positive",
-//			request: httptest.NewRequest(http.MethodGet, "/"+url.GetUUID(), nil),
-//			result: result{
-//				body:           "",
-//				headerLocation: url.GetFullURL(),
-//				code:           http.StatusTemporaryRedirect,
-//				contentType:    "",
-//			},
-//		},
-//		{
-//			name:    "test read url no id",
-//			request: httptest.NewRequest(http.MethodGet, "/", nil),
-//			result: result{
-//				body:           "",
-//				headerLocation: "",
-//				code:           http.StatusBadRequest,
-//				contentType:    "",
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			w := httptest.NewRecorder()
-//			handler.ServeHTTP(w, tt.request)
-//			res := w.Result()
-//
-//			assert.Equal(t, tt.result.code, res.StatusCode)
-//
-//			defer res.Body.Close()
-//			resBody, err := io.ReadAll(res.Body)
-//			require.NoError(t, err)
-//
-//			if tt.result.body != "" {
-//				assert.Equal(t, tt.result.body, string(resBody))
-//			}
-//
-//			if tt.result.contentType != "" {
-//				assert.Equal(t, tt.result.contentType, res.Header.Get("Content-Type"))
-//			}
-//		})
-//	}
-//}
 
 func TestNewHandler(t *testing.T) {
 	type args struct {
